e6ada63c31b97e67cf597dd5448a8b97cebc024a
Tanmaydevilhunter <tanmatnfsgta@gmail.com>
2024-06-09 17:56:06 -0400

    Input Output Streams complete

---
+++ Final_CPP_Project/3_Input_Output_Streams/Source.cpp
@@ -0,0 +1,6 @@
+//-There are lot of ways to take input and print to the console but the STL library has:-
+//-cin	- used for taking input
+//-cout	- used for printing to console
+//-cerr	- used for printing error to console
+
+//- It is present in <iostream> library and slower than printf
